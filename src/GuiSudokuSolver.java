5c7fa044fba5d79b467c53462c763956b36cd334
Benjscho <benjamin.j.schofield+coding@gmail.com>
2021-06-27 11:50:55 +0100

    Create basic JFrame for table
    Created basic JFrame window for table that displays Sudoku.
    
    Next steps are to:
    - Implement sudoku solver
    - Make table editable so sudoku can be entered
    - Add eventlistener that solves sudoku and prints it back

---
+++ src/GuiSudokuSolver.java
@@ -0,0 +1,41 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JTable;
+
+
+public class GuiSudokuSolver{
+    
+    public GuiSudokuSolver() {
+
+    }
+
+    public void draw() {
+        JFrame mFrame = new JFrame();
+        mFrame.setTitle("Sudoku Solver");
+        mFrame.setSize(640, 480);
+
+        SudokuTable st = new SudokuTable();
+        JTable table = new JTable(st);
+        mFrame.add(table);
+
+        mFrame.pack();
+        mFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        mFrame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        GuiSudokuSolver m = new GuiSudokuSolver();
+        m.draw();
+    }
+}
