651a733a9ebeb7371fd0e23572da1f19e02f27dd
Benjscho <benjamin.j.schofield+coding@gmail.com>
2021-06-27 18:11:44 +0100

    Initialisation function of solver
    Added the initialisation function of the solver. Takes the sudoku and
    translates it into a matrix of Dancing Links prior to solving.

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -1,8 +1,16 @@
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * SudokuSolver class
+ */
 public class SudokuSolver {
 
+    private Sudoku solution; 
+    private ArrayList<DLXNode> columnObject;
+    private DLXNode root; 
+    
     private class DLXNode {
         public DLXNode L, R, U, D, C; 
         public int value;
@@ -50,5 +58,79 @@ public class SudokuSolver {
             this.insertRow();
         }
 
+        public void cover() {
+            this.C.removeRow();
+            // Loop column
+            for (DLXNode n1 = this.D; n1 != this; n1 = n1.D) {
+                // Loop row
+                for (DLXNode n2 = n1.R; n2 != n1; n2 = n2.R) {
+                    n2.removeCol();
+                }
+            }
+        }
+
+        public void uncover() {
+            this.C.insertRow();
+            // Loop column
+            for (DLXNode n1 = this.D; n1 != this; n1 = n1.D) {
+                // Loop row
+                for (DLXNode n2 = n1.R; n2 != n1; n2 = n2.R) {
+                    n2.insertCol();
+                }
+            }
+        }
+    }
+
+    public SudokuSolver(Sudoku sudoku) {
+        solution = new Sudoku(); 
+
+        ArrayList<DLXNode> columnObject = new ArrayList<>();
+        for (int i = 0; i < 4*81; i++) {
+            columnObject.add(new DLXNode()); 
+        }
+
+        int cRow = 0; 
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                int sq = sudoku.getSquare(r, c);
+                if (sq == 0) {
+                    for (int x = 0; x < 9; x++) {
+                        addToMatrix(cRow, r, c, x);
+                    }
+                } else {
+                    addToMatrix(cRow, r, c, sq - 1);
+                }
+                cRow += 9; 
+            }
+        }
+
+        root = new DLXNode();
+        DLXNode last = root; 
+        for (DLXNode header : columnObject) {
+            header.insertRight(last);
+            last = header; 
+        }
+    }
+
+    private int[] constraints(int r, int c, int value) {
+        int cell = (9*r) + c;
+        int row = 81 + (9*r) + value;
+        int col = (81*2) + (9*c) + value;
+        int box = (81*3) + (9*((3*(r / 3)) + (c / 3))) + value; 
+
+        return new int[] {cell, row, col, box}; 
+    }
+
+    private void addToMatrix(int cRow, int r, int c, int value) {
+        DLXNode first = null; 
+        for (int con : constraints(r, c, value)) {
+            DLXNode node = new DLXNode(cRow + value);
+            node.insertAbove(columnObject.get(con));
+            if (first == null) {
+                first = node; 
+            } else {
+                node.insertRight(first);
+            }
+        }
     }
 }
